28b1f46407645d6538cb0291c4e8c557854ae069
AnnKravchenko <Anna.Kravchenko@enavate.com>
2020-08-21 18:30:01 +0300

    Hide non mandatory fields

--- Lead/Lead/server.js
+++ Lead/Lead/server.js
@@ -1,11 +1,26 @@
-//'use strict';
-//var http = require('http');
-//var port = process.env.PORT || 1337;
-
-//http.createServer(function (req, res) {
-//    res.writeHead(200, { 'Content-Type': 'text/plain' });
-//    res.end('Hello World\n');
-//}).listen(port);
 function hideFields() {
-
+    var topic = Xrm.Page.getAttribute("subject").getValue()
+    if (topic === null) {
+        var attributes = []
+        var ad = 'address1_composite'
+        Xrm.Page.data.entity.attributes.forEach(function (element, index) {
+            if (element.getName()!==ad)
+                attributes[index] = element.getName()
+        })
+        attributes.forEach(function (element, index) {
+            if (Xrm.Page.getControl(element) !== null || element.startsWith('address1_') ) {
+                if (Xrm.Page.getAttribute(element).getRequiredLevel() === "none") {
+                    if (element.startsWith('address1_')) 
+                        Xrm.Page.getControl('address1_composite_compositionLinkControl_'+element).setVisible(false)
+                    else
+                        Xrm.Page.getControl(element).setVisible(false)
+                } else {
+                    if (element.startsWith('address1_'))
+                        Xrm.Page.getControl('address1_composite_compositionLinkControl_' + element).setVisible(true)
+                    else
+                        Xrm.Page.getControl(element).setVisible(true)
+                }
+            }
+        })
+    }
 };
