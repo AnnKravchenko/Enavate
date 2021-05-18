b4d19cf770a93a3d271d92c8e5428771aa5fde73
AnnKravchenko <Anna.Kravchenko@enavate.com>
2021-05-18 13:39:11 +0300

    WebAPI

---
+++ WebAPI/WebAPI/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Data.SqlClient;
+
+
+namespace WebAPI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Set these values:
+            // e.g. https://yourorg.crm.dynamics.com
+            string url = "https://orgb5345919.api.crm4.dynamics.com";
+            var resourceUri = "a9bbe5b7-81d8-4686-bedf-2adbb1ad9134";
+            // e.g. you@yourorg.onmicrosoft.com
+            string userName = "anna.kravchenko@testwebapi2.onmicrosoft.com";
+            // e.g. y0urp455w0rd
+            string password = "TestWebAPI2";
+            
+            // Azure Active Directory registered app clientid for Microsoft samples
+            string clientIdq = "3cdb629e-5430-4ae1-b91e-f48a4ce72ce6";
+
+            var userCredential = new UserCredential(userName, password);
+            string apiVersion = "9.2";
+            string webApiUrl = $"{url}/api/data/v{apiVersion}";
+
+            var serviceUrl = "https://orgb5345919.api.crm4.dynamics.com";
+            var clientId = "1b6e9d73-912c-43fb-99a4-13bb0061fd87";
+            var secret = "hC16ek4mljOg.x~..M56c525~~wgCxlIM6";
+
+            var authCtx = new AuthenticationContext("https://login.microsoftonline.com/a9bbe5b7-81d8-4686-bedf-2adbb1ad9134");
+            var creds = new ClientCredential(clientId, secret);
+            var result = authCtx.AcquireToken(serviceUrl, creds);
+            var accessToken = result.AccessToken;
+
+            //Authenticate using IdentityModel.Clients.ActiveDirectory
+            //var authParameters = AuthenticationParameters.CreateFromResourceUrlAsync(new Uri(webApiUrl)).Result;
+            //var authContext = new AuthenticationContext(authParameters.Authority, false);
+            //var authResult = authContext.AcquireToken(resourceUri, clientId, userCredential);
+            //var authHeader = new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+            var authHeader = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            SQL s = new SQL();
+            JArray accounts = new JArray(s.ConnectionData);
+            //Console.WriteLine(accounts);
+
+            
+            for (int i = 0; i < accounts.Count; i++)
+            {
+                using (var client = new HttpClient())
+                {
+                
+                    
+                    try
+                    {
+                        client.BaseAddress = new Uri(webApiUrl);
+                        client.DefaultRequestHeaders.Authorization = authHeader;
+                        client.DefaultRequestHeaders.Add("OData-MaxVersion", "4.0");
+                        client.DefaultRequestHeaders.Add("OData-Version", "4.0");
+                        client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                        Console.WriteLine(accounts[i]);
+                        //Console.ReadLine();
+
+                        if (accounts[i]["accountid"] == null)//create record
+                        {
+                            client.DefaultRequestHeaders.Add("Prefer", "return=representation");
+                            HttpContent content = new StringContent(accounts[i].ToString(), Encoding.UTF8, "application/json"); //JObject of the data to be posted.
+                            HttpResponseMessage response = await client.PostAsync($"{client.BaseAddress}/accounts", content);
+                            string json = await response.Content.ReadAsStringAsync();
+                            JObject returnedData = JObject.Parse(json);
+                            Console.WriteLine(returnedData);
+                            //Console.ReadLine();
+                            s.UpdateSqlRecord(returnedData);
+                            Console.WriteLine(response.ToString());
+                        }
+                        else // update record
+                        {
+                            //client.DefaultRequestHeaders.Add("If-Match", "*");
+                            HttpContent content = new StringContent(accounts[i].ToString(), Encoding.UTF8, "application/json"); //JObject of the data to be posted.
+                            HttpResponseMessage response = await PatchAsync(client, $"{client.BaseAddress}/accounts({accounts[i]["accountid"].ToString()})", content);
+                            Console.WriteLine(response.ToString());
+                            Console.ReadLine();
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Exception: " + ex.Message);
+
+                    }
+                }
+            }
+            Console.ReadLine();
+            
+        }
+        public static async Task<HttpResponseMessage> PatchAsync(HttpClient client, string requestUri, HttpContent content)
+        {
+            var method = new HttpMethod("PATCH");
+            var request = new HttpRequestMessage(method, requestUri)
+            {
+                Content = content
+            };
+            Console.WriteLine("request\n "+request.ToString());
+            var response = await client.SendAsync(request);
+            return response;
+        }
+    }
+}
---
+++ WebAPI/WebAPI/SQL.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Newtonsoft.Json.Linq;
+
+namespace WebAPI
+{
+    class SQL
+    {
+        public SQL()
+        {
+            string connectionString = @"Data Source=PC-AKRAVCHENKO2;Initial Catalog = accounts;Integrated Security=SSPI;";
+            this.Connection = new SqlConnection(connectionString);
+        }
+        public SqlConnection Connection { get; set; }
+        public JArray ConnectionData
+        {
+            get
+            {
+                SqlConnection cnn = this.Connection;
+                JArray accs = new JArray();
+                try
+                {
+                    
+                    cnn.Open();
+                    Console.WriteLine("Connection Open ! ");
+                    Console.WriteLine(cnn.Database);
+                    using (SqlCommand command = new SqlCommand("SELECT  * FROM CLients", cnn))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                JObject acc = new JObject
+                                {
+                                    { "name",  reader["accountname"].ToString() },
+                                    { "new_cash", reader["new_cash"].ToString() },
+                                    { "emailaddress1", reader["emailaddress1"].ToString() }
+                                    //{ "parentaccountid@odata.bind", "/accounts(f8ccd9f5-26a4-eb11-b1ac-000d3ab2f628)"}
+                                };
+                                if (reader["accountid"].ToString() != "")
+                                {
+                                    acc.Add("accountid", reader["accountid"].ToString());
+                                }
+                                accs.Add(acc);
+                            }
+                        }
+                    }
+                    cnn.Close();
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("ConnectionData Exception: " + ex.Message);
+                    Console.ReadLine();
+                }
+                return accs;
+            }
+        }
+        public void UpdateSqlRecord(JObject acc)
+        {
+            SqlConnection cnn = this.Connection;
+            try
+            {
+
+                cnn.Open();
+                using (SqlCommand command = cnn.CreateCommand())
+                {
+                    command.CommandText = "update accounts.dbo.Clients set accountid=@id where accountname=@name and new_cash=@nc and emailadress1=@em";
+                    command.Parameters.AddWithValue("@id", acc["accountid"].ToString());
+                    command.Parameters.AddWithValue("@name", acc["name"].ToString());
+                    command.Parameters.AddWithValue("@nc", acc["new_cash"].ToString());
+                    command.Parameters.AddWithValue("@em", acc["emailaddress1"].ToString());
+                    command.ExecuteNonQuery();
+
+                }
+                cnn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("UpdateSqlRecord Exception: " + ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
